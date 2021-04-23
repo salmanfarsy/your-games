0bb0750e0675abd4d737c45532550850054b6c14
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-04-23 15:45:08 +0600

    done with the comments

--- routes/comment.js
+++ routes/comment.js
@@ -20,6 +20,34 @@ router.post('/games/:id/comment', (req, res)=>{
             })
         }
     })
-})
+});
+router.get('/games/:id/comment/:com/edit', (req, res)=>{
+            Comment.findById(req.params.com, (err, comment)=>{
+                if(err){
+                    console.log(err)
+                } else{
+                    res.render('comment/edit', {comment : comment, id : req.params.id})
+                }
+            })
+            
+        });
+router.put('/games/:id/comment/:com', (req, res)=>{
+    Comment.findByIdAndUpdate(req.params.com, req.body.comment, (err, done)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.redirect('/games/' + req.params.id)
+        }
+    })
+});
 
+router.delete('/games/:id/comment/:com', (req, res)=>{
+    Comment.findByIdAndDelete(req.params.com, (err, done)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.redirect('/games/' + req.params.id) 
+        }
+    })
+})
 module.exports = router;
